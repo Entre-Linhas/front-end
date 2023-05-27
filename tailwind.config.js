fb12142f68643ea67c5803b52c2fd1faa5f2f43e
Matheus Passos <125465372+DevPassosMatheus@users.noreply.github.com>
2023-05-27 18:56:44 -0300

    Atualizando a interface com o que já foi feito.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Sacramento': ['Sacramento', 'cursive'],
+        'Nunito': ['Nunito', 'sans-serif'],
+      },
+      colors: {
+        'custom-yellow': 'rgba(254, 209, 48, 0.09)', 
+        'custom-salmon': '#FF6464', 
+      },
+    },
   },
   plugins: [],
 }
-
