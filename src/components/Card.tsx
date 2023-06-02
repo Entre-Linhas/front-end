66c6225a3a187fba82a21ab2dbc811e754fde3a8
Matheus Passos da Silva <125465372+DevPassosMatheus@users.noreply.github.com>
2023-06-02 00:00:10 -0300

    att front

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,7 +11,7 @@ typeStyle?: "primary" | "secondary"
 
 export const Card = ({icon:Icon, title, text, typeStyle = "primary"}:CardProps) => {
 return (
-        <div className={`bg-white flex flex-col rounded-[20px] border-2 border-[#E3E3E3] text-center justify-evenly px-[1.6rem] ${typeStyle === "primary" ? "h-[23.1rem] w-[36.2rem]" : "h-[30.8rem] w-[27.3rem]"}`}>
+        <div className={`bg-white flex flex-col rounded-[20px] border-2 border-[#E3E3E3] text-center justify-evenly px-[1.6rem] ${typeStyle === "primary" ? "basis-2/4 max-w-[36.2rem]" : "basis-2/4 min-h-[30.8rem] max-w-[27.3rem]"}`}>
           {Icon && <Icon className="text-custom-salmon self-center" size="50" />}
           <div className="flex flex-col gap-[2rem]">
             <h1 className="font-bold text-[2.2rem] font-[Nunito]">{title}</h1>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,73 +8,162 @@ import { Image } from "../components/Image"
 
 export default function Home() {
     return (
+      
+      //   <div className="box-border w-[100%] m-auto px-[1.5rem] sm:max-w-[] md:max-w-[] lg:max-w-[] xl:max-w-[1220px] 2xl:max-w-[1476px]"> </div> -- da pra usar de container
+      
       <div>
 
       <Header />
 
-     
-      <div className="bg-custom-yellow h-[auto] flex items-center justify-between px-[60px]"> 
+      
+      <div className="bg-custom-yellow h-[auto] justify-betweenWBG"> 
+      <div className="bg-black flex box-border w-[100%] m-auto px-[1.5rem] sm:max-w-[] md:max-w-[] lg:max-w-[] xl:max-w-[1220px] 2xl:max-w-[1476px]">
           <div className="flex flex-col gap-[1.6rem]">
               <h1 className='text-[3.6rem] font-[Nunito] font-bold max-[800px]:text-[1rem]'>Conosco você poderá costurar o <br/>seu futuro!</h1>
               <p className='text-[2.2rem]'>O Entre-Linhas foi desenvolvido com foco total em você,<br/> e no seu negócio! Explore a plataforma, aprenda e<br/>evolua!</p>
               <Button title='Comece agora!' typeStyle='secondary' />
           </div>
-             <img className="self-end" src='/Banner1.svg' alt="Imagem de uma moça costurando"></img>
+             <img className="" src='/Banner1.svg' alt="Imagem de uma moça costurando"></img>
       </div>
+      </div>
+      {/* to aqui */}
        
-      <div className="m-[-45px] w-[100%]">
-          <div className="flex justify-between w-[80%] m-auto max-[600px]:w-[100%]">
-            <Card title="Aprenda a empreender" text="Aqui você encontrará tudo que precisa para transformar o seu negócio!" icon={Bag} />
-            <Card title="Conheça ferramentas" text="Problemas com o gerenciamento? A gente te ajuda! " icon={Hammer}/>
-            <Card title="Conheça outras pessoas" text="Que tal criar conexões com pessoas tão incríveis quanto você?" icon={Bag}/>
-          </div>
+        <div className="m-[-45px] w-[100%]">
+             <div className="box-border gap-[65px] flex justify-between box-border w-[100%] h-[23.1rem] m-auto xl:max-w-[1220px] 2xl:max-w-[1476px]">
+               <Card title="Aprenda a empreender" text="Aqui você encontrará tudo que precisa para transformar o seu negócio!" icon={Bag} />
+               <Card title="Conheça ferramentas" text="Problemas com o gerenciamento? A gente te ajuda! " icon={Hammer}/>
+               <Card title="Conheça outras pessoas" text="Que tal criar conexões com pessoas tão incríveis quanto você?" icon={Bag}/>
+            </div>
+        </div>
+
+        {/* ---------------------------------------- */}
+
+      <div className="box-border w-[100%] m-auto px-[1.5rem] sm:max-w-[] md:max-w-[] lg:max-w-[] xl:max-w-[1220px] 2xl:max-w-[1476px]">
+
+
+        <div className="flex items-center px-[9.6rem] justify-between py-[10rem]">
+          <div className="w-[50%]">
+              <h1 className="text-[4.8rem] font-[Nunito] text-custom-salmon">"Costure seu futuro" <span className="text-black">aqui</span></h1>
+              <p className="text-[2.2rem]">Conteúdo totalmente produzido com foco em você e para se adaptar ao seu contexto e realidade. Conosco, a teoria e a prática vão caminhar juntas, construa seu negócio enquanto aprende de forma intuitiva e interativa!</p>
+            </div>
+            <Image source="Ellipse 65.svg"/>
+        </div>
+
+
       </div>
 
-      <Container image="Ellipse 65.svg" text="Conteúdo totalmente produzido com foco em você e para se adaptar ao seu contexto e realidade. Conosco, a teoria e a prática vão caminhar juntas, construa seu negócio enquanto aprende de forma intuitiva e interativa!" title="Costure seu futuro" perword="aqui" percolor="text-black"/>
+      {/* <Container image="Ellipse 65.svg" text="Conteúdo totalmente produzido com foco em você e para se adaptar ao seu contexto e realidade. Conosco, a teoria e a prática vão caminhar juntas, construa seu negócio enquanto aprende de forma intuitiva e interativa!" title="Costure seu futuro" perword="aqui" percolor="text-black"/> */}
       
+
+      {/* ------------------------------------------ */}
     
 
-         <div className=" h-[50rem] flex flex-col justify-center gap-[74px]">
+         <div className="h-[50rem] flex flex-col justify-center gap-[74px]">
             <h1 className="m-auto font-[Nunito] text-[48px]">Conheça nossa <span className="text-custom-salmon">trilha educacional</span></h1>
-            <div className="z-10 flex px-[50px] justify-center m-auto items-center">
+            <div className="flex justify-center m-auto items-center">
               <Card typeStyle="secondary" title="Conheça outras pessoas" text="Que tal criar conexões com pessoas tão incríveis quanto você?" icon={Bag}/>
-              <div className="border-dashed border-t-[2px] border-custom-salmon w-[7rem]"></div>
+              <div className="border-dashed border-t-[2px] border-custom-salmon w-[9rem]"></div>
               <Card typeStyle="secondary" title="Conheça outras pessoas" text="Que tal criar conexões com pessoas tão incríveis quanto você?" icon={Hammer}/>
-              <div className="border-dashed border-t-[2px] border-custom-salmon w-[7rem]"></div>
+              <div className="border-dashed border-t-[2px] border-custom-salmon w-[9rem]"></div>
               <Card typeStyle="secondary" title="TESTE TESTE" text="Que tal criar conexões com pessoas tão incríveis quanto você?" icon={Bag}/>
-              <div className="border-dashed border-t-[2px] border-custom-salmon w-[7rem]"></div>
+              <div className="border-dashed border-t-[2px] border-custom-salmon w-[9rem]"></div>
               <Card typeStyle="secondary" title="Conheça outras pessoas" text="Que tal criar conexões com pessoas tão incríveis quanto você?" icon={Bag}/>
             </div>   
          </div>
 
-        <Container image="Ellipse 65.svg" text="Até a mais bela roupa passou pela mão de um profissional de costura. Cada ponto, cada traço, cada corte, importa. " title="" percolor="text-black" perword="Por que fazemos?" />
-
-        <div className="h-[509px] bg-[#CCCCCC]">
+         <div className="box-border w-[100%] m-auto px-[1.5rem] sm:max-w-[] md:max-w-[] lg:max-w-[] xl:max-w-[1220px] 2xl:max-w-[1476px]">
 
 
+        <div className="flex items-center px-[9.6rem] justify-between py-[10rem]">
+          <div className="w-[50%]">
+              <h1 className="text-[4.8rem] font-[Nunito] text-custom-salmon">"Costure seu futuro" <span className="text-black">aqui</span></h1>
+              <p className="text-[2.2rem]">Conteúdo totalmente produzido com foco em você e para se adaptar ao seu contexto e realidade. Conosco, a teoria e a prática vão caminhar juntas, construa seu negócio enquanto aprende de forma intuitiva e interativa!</p>
+            </div>
+            <Image source="Ellipse 65.svg"/>
         </div>
 
+
+      </div>
+
+        <div className="h-[509px] bg-[#CCCCCC] text-[3.2rem]">
+            
+          
+       
+        </div>
+
+        {/* width: 100%;
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+        margin-left: auto; */}
+
+        {/* Container2 */}
+
         <div className="flex px-[12rem] py-[6rem] justify-between items-center">
-          <div className="w-[50%]">
+          <div className="w-[50%] flex flex-col gap-[1.6rem]">
             <h1 className="text-[4.8rem]">Conheça o <br/>
-            Entre  Linhas Podcast</h1>
+            <span className="font-[Sacramento] text-custom-salmon">Entre  Linhas</span> Podcast</h1>
             <p className="text-[2.2rem]">Histórias inspiradoras, para te inspirar! Conheça a trajetória de outros profissionais de costura que venceram desafios supreendentes, aprenda com experiências de vida e relatos reais. Vem pro PodLinhas!</p>
-            <Button typeStyle="secondary" title="Ouça aqui"/>
+            <Button typeStyle="primary" title="Ouça aqui"/>
           </div>
           <Image source="Ellipse 65.svg"/>
         </div>
 
-        <div className="bg-black py-[60px]">
-          <img src="/cellphone 1.svg"></img>
+        {/* Container 3 podcast */}
+
+        <div className="bg-red-100 py-[60px] flex justify-center gap-[5%]">
+
+          <img src="/cellphone.svg"></img>
+          
+          <div className="flex flex-col gap-[16px] items-start w-[40%]">
+            <h1 className="text-[4.8rem]"> Aprenda também pelo <span className="text-custom-salmon">App</span></h1>
+            <p className="text-[2.2rem]">Você escolhe como quer estudar! O Entre-Linhas foi feito para você e por você. Carregue o conhecimento e o poder de transformar o seu negócio em qualquer lugar. </p>
+            <img src="/Login.svg" className="h-[6.3rem]"/>
+          </div>
 
         </div>
 
+{/* footer */}
+        <div className="h-[235px] bg-[#C7C7C7]">
+        <div className="flex justify-evenly text-[2.2rem] py-[2rem]">
+              <nav>
+                <ul>teste</ul>
+                <ul>teste</ul>
+                <ul>teste</ul>
+                <ul>teste</ul>
+              </nav>
 
-        <div className="h-[235px] bg-gray-300">
+              <div>
+                <p>lorem</p>
+                <p>lorem</p>
+                <p>lorem</p>
+                <p>lorem</p>
+              </div>
+
+  <nav>
+                <p>lorem</p>
+                <p>lorem</p>
+                <p>lorem</p>
+                <p>lorem</p>
+              </nav>
+              
+
+              <nav>
+                <p>lorem</p>
+                <p>lorem</p>
+                <p>lorem</p>
+                <p>teste</p>
+              </nav>
+              
+              <div>
+                <p>redes sociais</p>
+              </div>
+            </div>
 
 
+            <div>teste</div>
         </div>
-
+       
 
     </div>
     )
