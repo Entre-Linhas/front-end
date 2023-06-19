8d5f9eb126f53f0e1a66841ebe0129114ba0d36d
Vinicius Costa <SkinzinSTG_@hotmail.com>
2023-06-19 01:49:32 -0300

    Update SignIn.tsx

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,6 +3,7 @@ import { Button } from "../components/Button";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Input } from "../components/Input";
+import { Logo } from "../components/Logo";
 
 export default function SignIn() {
     const [email, setEmail] = useState<string>("");
@@ -47,12 +48,12 @@ export default function SignIn() {
                     </header>
 
                     <div className="flex flex-col items-center my-auto gap-[1rem]">
-                        <span className="font-[Sacramento] text-custom-salmon text-[3.2rem] min-[360px]:text-[4.2rem] min-[560px]:text-[5.2rem] ">Entre Linhas</span>
+                        <Logo />
 
                         <h4 className="text-[1.4rem] min-[300px]:text-[1.6rem] min-[360px]:text-[1.8rem] min-[560px]:text-[2.2rem]">Olá, boas vindas de volta =)</h4>
                         <span className="text-[1rem] min-[300px]:text-[1.2rem] min-[360px]:text-[1.4rem] min-[720px]:text-[1.5rem]">Estamos contente em vê-lo(a) novamente.</span>
-                        <Input className="min-[220px]: w-full min-[720px]:max-w-[80%]" placeholder="Email" onChange={(e: any) => setEmail(e.target.value)} leftElement={<Envelope className="mr-2 text-zinc-300" weight="light" size={31} />} />
-                        <Input className="min-[220px]: w-full min-[720px]:" placeholder="Senha" leftElement={<Key className="mr-2 text-zinc-300" weight="light" size={31} />} />
+                        <Input className="w-full" placeholder="Email" onChange={(e: any) => setEmail(e.target.value)} leftElement={<Envelope className="mr-2 text-zinc-300" weight="light" size={31} />} />
+                        <Input className="w-full" placeholder="Senha" leftElement={<Key className="mr-2 text-zinc-300" weight="light" size={31} />} />
                         <Button title="Entrar" />
                     </div>
                 </div>
