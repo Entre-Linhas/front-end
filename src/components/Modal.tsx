3513af1a5b70e96543cfbf359a23d04884e6feba
Vinicius Costa <SkinzinSTG_@hotmail.com>
2023-06-21 16:16:17 -0300

    add: Modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,22 @@
+import { HTMLAttributes } from "react";
+import { X } from "@phosphor-icons/react"
+
+
+interface ModalProps extends HTMLAttributes<HTMLDivElement> {
+    children: React.ReactNode;
+    _showModal: boolean
+    _close: () => void;
+}
+
+export function Modal({ children, _showModal, _close, ...rest }: ModalProps) {
+    return (
+        <div className={`fixed flex items-center justify-center bg-black bg-opacity-50 top-0 right-0 bottom-0 left-0 transition-all duration-200 ${!_showModal && "invisible opacity-0"}`}>
+            <div className="flex flex-col relative max-w-2xl bg-gray-100 max-h-screenModal rounded-lg shadow-md px-8 py-7" {...rest}>
+                <button onClick={_close} className="ml-auto">
+                    <X />
+                </button>
+                {children}
+            </div>
+        </div>
+    )
+}
--- src/pages/Nivelamento.tsx
+++ src/pages/Nivelamento.tsx
@@ -1,46 +1,50 @@
-import { Header } from "../components/Header";
-import { Button } from "../components/Button";
-import { Footer } from "../components/Footer";
-import { InstagramLogo } from "@phosphor-icons/react";
-import { Card } from "../components/Card"
-import { IconProps } from "@phosphor-icons/react"
+import { useState } from "react";
 import { Star, StarHalf } from "@phosphor-icons/react";
+import { Header } from "../components/Header";
+import { Modal } from "../components/Modal";
 
 export default function Nivelamento () {
-return (
+    const [showModal, setShowModal] = useState(false);
 
-<div className="bg-white">
-
-    <Header />
-  
-        
-        <div className="py-[24.5rem] flex flex-col gap-[10rem] px-[2.5rem]">
-        <div className="margin-auto flex items-center justify-center gap-[9rem] max-[1023px]:flex-col">
-                <div className="border solid border-1 border-[#E3E3E3] bg-white rounded-[2rem] shadow-mg flex flex-col items-center text-center min-[1024px]:min-h-[48.7rem] min-[1024px]:max-w-[39.87rem]">
-                    <div className="py-[6rem] flex flex-col items-center gap-[5.2rem] px-[3.8rem]">
-                        <StarHalf size="50" className="text-custom-salmon"/>
-                            <div>
-                                <h2 className="font-bold text-[2.2rem]">Vou começar meu negócio</h2>
-                                <p className="text-center">Aqui você começa a trilha desde o início, vendo todos os tópicos</p>
+    function handleModal() {
+        setShowModal(!showModal)
+    }
+    return (
+        <>
+            <div>
+                <Header />
+                <div className="py-[24.5rem] flex flex-col gap-[10rem] px-[2.5rem]" >
+                    <div className="margin-auto flex items-center justify-center gap-[9rem] max-[1023px]:flex-col" style={{overflow: showModal ? "auto" : "hidden"}}>
+                        <div className="border solid border-1 border-[#E3E3E3] bg-white rounded-[2rem] shadow-mg flex flex-col items-center text-center min-[1024px]:min-h-[48.7rem] min-[1024px]:max-w-[39.87rem]">
+                            <div className="py-[6rem] flex flex-col items-center gap-[5.2rem] px-[3.8rem]">
+                                <StarHalf size="50" className="text-custom-salmon"/>
+                                    <div>
+                                        <h2 className="font-bold text-[2.2rem]">Vou começar meu negócio</h2>
+                                        <p className="text-center">Aqui você começa a trilha desde o início, vendo todos os tópicos</p>
+                                    </div>
+                                <button className="bg-custom-salmon text-white py-[1rem] px-[1rem] rounded-[0.5rem] text-[1.8rem] min-[320px]:px-[3rem] min-[360px]:px-[5rem] min-[425px]:px-[8rem] min-[530px]:px-[10rem]">Iniciar Trilha</button>
                             </div>
-                       <button className="bg-custom-salmon text-white py-[1rem] px-[1rem] rounded-[0.5rem] text-[1.8rem] min-[320px]:px-[3rem] min-[360px]:px-[5rem] min-[425px]:px-[8rem] min-[530px]:px-[10rem]">Iniciar Trilha</button>
-                    </div>
-                </div>
-                <div className="border solid border-1 border-[#E3E3E3] bg-white rounded-[2rem] shadow-mg flex flex-col items-center text-center min-[1024px]:min-h-[48.7rem] min-[1024px]:max-w-[39.87rem]">
-                    <div className="py-[6rem] flex flex-col items-center gap-[5.2rem] px-[3.8rem]">
-                        <Star size="50" className="text-custom-salmon"/>
-                            <div>
-                                <h2 className="font-bold text-[2.2rem]">Já tenho meu negócio</h2>
-                                <p className="text-center">Aqui você faz um teste para pular o conteúdo que você já sabe</p>
+                        </div>
+                        <div className="border solid border-1 border-[#E3E3E3] bg-white rounded-[2rem] shadow-mg flex flex-col items-center text-center min-[1024px]:min-h-[48.7rem] min-[1024px]:max-w-[39.87rem]">
+                            <div className="py-[6rem] flex flex-col items-center gap-[5.2rem] px-[3.8rem]">
+                                <Star size="50" className="text-custom-salmon"/>
+                                    <div>
+                                        <h2 className="font-bold text-[2.2rem]">Já tenho meu negócio</h2>
+                                        <p className="text-center">Aqui você faz um teste para pular o conteúdo que você já sabe</p>
+                                    </div>
+                                <button className="bg-custom-salmon text-white py-[1rem] px-[1rem] rounded-[0.5rem] text-[1.8rem] min-[320px]:px-[3rem] min-[360px]:px-[5rem] min-[425px]:px-[8rem] min-[530px]:px-[10rem]">Iniciar Trilha</button>
                             </div>
-                       <button className="bg-custom-salmon text-white py-[1rem] px-[1rem] rounded-[0.5rem] text-[1.8rem] min-[320px]:px-[3rem] min-[360px]:px-[5rem] min-[425px]:px-[8rem] min-[530px]:px-[10rem]">Iniciar Trilha</button>
+                        </div>
                     </div>
+                    <button onClick={handleModal} className="mx-0 text-center text-[2.2rem] underline text-[#525151]">Entenda mais sobre o teste de nivelamento e por onde começar.</button>
                 </div>
-        </div>
-            <div className="mx-0 text-center text-[2.2rem] underline text-[#525151]">Entenda mais sobre o teste de nivelamento e por onde começar.</div>
-        </div>
-   
-</div>
- 
-)
+            </div>
+            <Modal _showModal={showModal} _close={handleModal}>
+                <div className="flex flex-col overflow-auto gap-8 mx-14 my-10">
+                    <h2 className="text-custom-salmon font-semibold text-3xl text-center">Com dúvida em qual opção escolher?</h2>
+                    <p className="">Se você pretende iniciar um negócio, a primeira opção foi feita para você. Ela irá te ensinar desde como identificar seu cliente até o que o seu produto tem de especial.<br/><br/>Já se você tem um negócio, a segunda opção é a melhor. Você poderá escolher em que ponto da trilha irá iniciar seus estudos.</p>
+                </div>
+            </Modal>
+        </>
+    )
 }
