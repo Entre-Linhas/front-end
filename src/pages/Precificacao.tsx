d188ba434ce552b1fe86d242e6fc193b91a91c33
Thailany Pereira <111004514+ThailanyP@users.noreply.github.com>
2023-06-24 16:35:53 -0300

    Add files via upload

--- src/pages/Precificacao.tsx
+++ src/pages/Precificacao.tsx
@@ -2,7 +2,7 @@ import { Header } from "../components/Header";
 import { Button } from "../components/Button";
 import { Linking } from '../components/Linking';
 import { Footer } from "../components/Footer";
-import { Acordes } from "../components/Acordes";
+import { Accordion } from "../components/Accordion";
 
 export default function Precificacao() {
     return( 
@@ -13,7 +13,7 @@ export default function Precificacao() {
                     {/* container */}
                     <div className="container mx-auto">
                             {/* layout de grade com três linhas e fluxo de coluna, com um espaçamento de 4 unidades  */}
-                        <div className="grid grid-rows-3 grid-flow-col gap-4">
+                        <div className="grid grid-row-3 grid-flow-col gap-4">
                             {/* row-span  é linha vertical, e aqui ocupa 3, com sombra e borda aredondada*/}
                             <div className="row-span-3 h-48 w-96 shadow-lg rounded-lg">
                                 {/* margin-left */}
@@ -21,10 +21,6 @@ export default function Precificacao() {
                                 <div className="ml-4 ">
                                     <nav>
                                         <ul>
-                                            {/*Linking cria rotas */}
-                                        {/* <Linking to="/precificacao" title="Precificação"/>
-                                            <Linking to="/gerenciamento-de-pedidos" title="Gerenciamento de pedidos"/>
-                                            <Linking to="/fornecedores" title="Fornecedores"/> */}
                                             <li>Precificação</li>
                                             <li>Gerenciamento de pedidos</li>
                                             <li>Fornecedores</li>
@@ -33,27 +29,31 @@ export default function Precificacao() {
                                 </div>
                             </div>
 
-                            <div className="">
-                                    <h3>Precificação</h3>
+                            <div className="ml-20">
+                                    <h3 className=""><b>Precificação</b></h3>
                                     <p>Adicione os itens usados no seu produto e calcule.</p>
-                            </div>
 
-                            <div className="bg-white shadow-lg rounded-lg" >
-                                <div className="m-10">
-                                    <div className="m-1">
-                                        <p>Nome do produto: Bolsa de mão</p>
-                                    </div>
+                                    <div className="bg-white shadow-lg rounded-lg" >
+                                        <div className="m-12 mt-5">
+                                            <div className="">
+                                                {/* aqui o usuario tem que digitar */}
+                                                <p> <b>Nome do produto:</b> Bolsa de mão</p>
+                                                <br></br>
+                                            </div>
 
-                                    <div>
-                                        <h3>Materias usados</h3>
-                                        {/* accordion */}
-                                            <Acordes/>
+                                            <div>
+                                                <h3><b>Materias usados</b></h3>
+                                                {/* accordion */}
+                                                    <Accordion/>
+                                            </div>
+                                            <div className="mt-2 mb-5">
+                                                <button className="bg-[#62B8AF] w-full text-white py-5 rounded-0.5rem">+ Novo item</button>
+                                            </div>
+                                            <div className="mt-2 mb-5">
+                                                <button className="bg-[#62B8AF] w-full text-white py-5 rounded-0.5rem">Calcular</button>
+                                            </div>
+                                        </div>
                                     </div>
-
-                                    <div className="mt-2">
-                                        <Button title="Calcular"/>
-                                    </div>
-                                </div>
                             </div>
 
                         </div>
