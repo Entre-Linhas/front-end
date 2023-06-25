5233d91729a0bb9c9c940fca4bac78ff870a9fc8
Vinicius Costa <SkinzinSTG_@hotmail.com>
2023-06-24 23:46:20 -0300

    feat: Props Model

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,26 +1,27 @@
-import { CheckCircle } from "@phosphor-icons/react"
-import { IconProps } from "@phosphor-icons/react"
+import { Check } from "@phosphor-icons/react";
 
-interface ModelProps {
-  title: string
-  describe: string
-  time: string
-  icon?: React.FC<IconProps>;
+export interface ModelProps {
+  title: string;
+  describe: string;
+  time: string;
+  _completed?: boolean;
 }
 
 
-export const Model = ({ icon: Icon, title, describe, time }: ModelProps) => {
+export const Model = ({ title, describe, time, _completed = false }: ModelProps) => {
   return (
     <div>
 
       <div className="flex gap-[4rem]">
-        <div className="flex gap-[0.5rem]">
-          <p className="font-bold">{title}</p>
-          {Icon && <Icon size={22} weight="fill" className="text-green-600" />}
+        <div className="flex items-center gap-[0.5rem] w-full">
+          <p className={`font-bold ${!_completed && "opacity-50"}`}>{title}</p>
+          {_completed && <div className="bg-green-500 rounded-full p-1">
+            <Check  weight="bold"/>
+          </div>}
+          <span className="text-[1.4rem] text-[#A1A1AA] ml-auto">{time}</span>
         </div>
-        <span className="text-[1.4rem] text-[#A1A1AA]">{time}</span>
       </div>
-      <span>{describe}</span>
+      <span className={`${!_completed && "opacity-50"}`}>{describe}</span>
     </div>
   )
 }
