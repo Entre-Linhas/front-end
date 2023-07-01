63457f341839c41d66d709aa99d0edc9c24790ca
Vinicius Costa <SkinzinSTG_@hotmail.com>
2023-07-01 19:32:28 -0300

    fix: Erro ao definir a Key do Icone.

--- src/components/RedeSocial.tsx
+++ src/components/RedeSocial.tsx
@@ -1,26 +1,30 @@
 import { FacebookLogo, TwitterLogo, TwitchLogo, InstagramLogo, WhatsappLogo, TelegramLogo } from "@phosphor-icons/react"
+import { useEffect } from "react";
 
 
 export interface RedeSocialProps {
     url: string;
 }
 
+type IconName = {
+    [key: string]: React.ReactElement;
+}
 
 export function RedeSocial({ url }: RedeSocialProps) {
     const getLinkingName = new RegExp('https://([^.]+).', 'g').exec(url.replace("www.", "")) || "vazio" // Pegando qual quer Caracters que não sejam https:// e .
    
-    const iconName = {
-        facebook: <FacebookLogo />,
-        twitter: <TwitterLogo />,
-        twitch: <TwitchLogo />,
-        instagram: <InstagramLogo />,
-        whatsapp: <WhatsappLogo />,
-        telegram: <TelegramLogo />
+    const iconName: IconName = {
+        "facebook": <FacebookLogo />,
+        "twitter": <TwitterLogo />,
+        "twitch": <TwitchLogo />,
+        "instagram": <InstagramLogo />,
+        "whatsapp": <WhatsappLogo />,
+        "telegram": <TelegramLogo />
     }
 
     return (
         <div>
-            {iconName[getLinkingName !== "vazio" && getLinkingName[1]] || <div className="w-2 h-2 rounded-md bg-gray-400" />}
+            {iconName[getLinkingName[1]] || <div className="w-2 h-2 rounded-md bg-gray-400" />}
 
             <span>{getLinkingName === "vazio" ? "Not found" : getLinkingName[1].charAt(0).toUpperCase() + getLinkingName[1].slice(1).toLowerCase()}</span>
         </div>
